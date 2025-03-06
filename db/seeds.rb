ea4cf81b876ebde1d4f0c73f672a8fd7fd86d31e
Manpreet S <manpreeet1.singh@trantorinc.com>
2025-03-06 11:53:35 -0500

    BMS | Added book data for initial population of books

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,41 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+# Add sample data for books
+Book.destroy_all
+
+Book.create!(
+  title: "The Jungle Book",
+  total_copies: 20,
+  available_copies: 20,
+  created_at: Time.now,
+  updated_at: Time.now,
+  fee: 2.0
+)
+
+Book.create!(
+  title: "Another Book",
+  total_copies: 15,
+  available_copies: 15,
+  created_at: Time.now,
+  updated_at: Time.now,
+  fee: 1.5
+)
+
+Book.create!(
+  title: "Sample Book",
+  total_copies: 15,
+  available_copies: 15,
+  created_at: Time.now,
+  updated_at: Time.now,
+  fee: 5
+)
+
+Book.create!(
+  title: "Test Book",
+  total_copies: 10,
+  available_copies: 10,
+  created_at: Time.now,
+  updated_at: Time.now,
+  fee: 11
+)
